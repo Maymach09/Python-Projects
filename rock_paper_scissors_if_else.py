1ff7b82c83dc8a6af6a5166d6ba6c3b5a43d7228
Maymach09 <mayank.mahajan0@gmail.com>
2024-01-15 19:54:45 -0400

    Update rock_paper_scissors_if_else.py

--- rock_paper_scissors_if_else.py
+++ rock_paper_scissors_if_else.py
@@ -1,3 +1,5 @@
+import random #random number generator module
+
 rock = '''
     _______
 ---'   ____)
@@ -25,55 +27,22 @@ scissors = '''
 ---.__(___)
 '''
 
-import random #random number generator module
+choices = [rock,paper,scissors]
 
-#Fetching user's input
+#Fetching and displaying user's input
 user_input = int(input("What do you chose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
+print("User chose:\n", choices[user_input])
 
-#Fetching computer's input
+#Fetching and dispaying computer's input
 computer_input = random.randint(0,2)
-
-#display user's selection
-if user_input == 0:
-    print("You chose:\n\n", rock)
-elif user_input == 1:
-    print("You chose:\n\n", paper)
-else:
-    print("You chose:\n\n", scissors)
-
-#display computer's selection
-if computer_input == 0:
-    print("Computer chose:\n\n", rock)
-elif computer_input == 1:
-    print("Computer chose:\n\n", paper)
-else:
-    print("Computer chose:\n\n", scissors)
+print("Computer chose:\n", choices[computer_input])
 
 #winning logic
-if user_input == 0:
-    if computer_input == 0:
+if user_input == computer_input:
         print("It's a draw")
-    elif computer_input == 1:
-        print("You lose")
-    else:
-        print("You win")
-
-elif user_input == 1:
-    if computer_input == 1:
-        print("It's a draw")
-    elif computer_input == 2:
-        print("You lose")
-    else:
-        print("You win")
-
-elif user_input == 2:
-    if computer_input == 2:
-        print("It's a draw")
-    elif computer_input == 0:
-        print("You lose")
-    else:
-        print("You win")  
-
+elif (user_input == 0 and computer_input == 1) or (user_input == 1 and computer_input == 2) or (user_input == 2 and computer_input == 0):
+        print("\nComputer Wins!")
+elif (user_input == 2 and computer_input == 1) or (user_input == 1 and computer_input == 0) or (user_input == 0 and computer_input == 2):
+        print("\nYou Win!")
 else:
-    print("Invalid Input")      
-    
+    print("\nInvalid input, you lose")
