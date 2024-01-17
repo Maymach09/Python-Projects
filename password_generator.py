7721476ea1ef70748fcce5999a70440f51b152d5
Maymach09 <mayank.mahajan0@gmail.com>
2024-01-17 11:49:52 -0500

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -9,24 +9,49 @@ characters = [letters, numbers, symbols]
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
 
 total_char = nr_letters + nr_numbers + nr_symbols
 
-#Create a list of random letters
+#Create a string of random letters
 random_char = ""
-random_list = []
+letter_count = 0
+num_count = 0
+sym_count = 0
+
+for out_num in range(1, total_char):
+    ran_no_outer_list = random.randint(0, len(characters)-1)
+    
+    print(f"Loop: {out_num}")
+
+    if ran_no_outer_list == 0:
+        if letter_count > nr_letters:
+            continue
+        else:
+            ran_no_inner_list = random.randint(0, len(letters)-1)
+            random_char += characters[ran_no_outer_list][ran_no_inner_list]
+            letter_count += 1
+            print("letter: ", random_char)
+
+    elif ran_no_outer_list == 1:
+        if num_count > nr_numbers:
+            continue
+        else:
+            ran_no_inner_list = random.randint(0, len(numbers)-1)
+            random_char += characters[ran_no_outer_list][ran_no_inner_list]
+            num_count += 1
+            print("number: ", random_char)
+        
+    elif ran_no_outer_list == 2:
+        if sym_count > nr_symbols:
+            continue
+        else:
+            ran_no_inner_list = random.randint(0, len(symbols)-1)
+            random_char += characters[ran_no_outer_list][ran_no_inner_list]
+            sym_count += 1
+            print("symbol: ", random_char)
 
 
-for list in characters:
-    # if len(random_char) == total_char:
-    #     break
-    # else:
-    ran_no_list = random.randint(0,len(characters)-1)
-    ran_no_letters = random.randint(0, len(letters)-1)
-    ran_no_numbers = random.randint(0, len(numbers)-1)
-    ran_no_symbols = random.randint(0, len(symbols)-1)
-    random_list += characters[ran_no_list][ran_no]
-
-print(random_list)
+print(f"Here is the  password: {random_char}")
+    
