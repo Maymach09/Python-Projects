bc8d78332b1454e95c2c8495ca05a40944896389
Maymach09 <mayank.mahajan0@gmail.com>
2024-01-20 05:45:46 -0500

    added more python files to import

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,67 +1,13 @@
 import random
+import hangman_art
+import hangman_words
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 display = []
 lives = 6
 end_of_game = False
+
+print(hangman_art.logo)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -72,24 +18,33 @@ print(display)
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print("You have already entered this letter, please chose a different letter")
+    
+    #Check guessed letter
     for rcount,letter in enumerate(chosen_word):
         if letter == guess:
             display[rcount] = letter
     
+    #Check if user is wrong.
     if guess not in chosen_word:
+        print("Sorry! you have guessed the wrong letter, please chose another letter.")
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You Lose")
+            print("\nYou Lose")
     
     #Join all the elements in a list and turn it into a string
     print(f"{' '.join(display)}")
     
+    #check if user has got all letters
     if '_' not in display:
         end_of_game = True
         print("You Won.")
 
-    print(stages[lives])
+    #Print hangman after every guess
+    print(hangman_art.stages[lives])
 
 
     
