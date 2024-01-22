aca4204dfacc8a20877cd19b8243a8c002f090ee
Maymach09 <mayank.mahajan0@gmail.com>
2024-01-22 11:28:22 -0400

    Optimized Code for silent bidding

--- silent_bidding/bidding.py
+++ silent_bidding/bidding.py
@@ -1,39 +1,40 @@
 from art import logo
 import os
 
-print("Welcome to the secret auction program.")
-bid_item = {}
+print(logo)
+
+print("Welcome to the secret auction program.\n")
+bid_items = {}
 
 def clear_screen():
     if os.name == 'nt':
         os.system('cls')
-    else:
-        os.system('clear')
 
 def bid_winner(bid_data):
     max_bid = 0
     bidder = ""
-    for person in bid_data.keys():
-        if int(bid_data[person]) > int(max_bid):
+    for person, bid in bid_data.items():
+        if bid > max_bid:
             bidder = person
-            max_bid = bid_data[person]
-    print(f"Winner is {person} with a bid of {max_bid}.")
+            max_bid = bid
+    print(f"The winner is {bidder} with a bid of {max_bid}")
 
 should_continue = True
 while should_continue:
     person_name = input("What is your name?: ")
-    person_bid = input("What's your bid: $")
-    bid_item[person_name] = person_bid
-    continue_bidding = input("Are there any other bidders? Type 'yes' or 'no'.")
-    if continue_bidding == 'yes':
+    person_bid = int(input("What's your bid: $"))
+    bid_items[person_name] = person_bid
+
+    continue_bidding = input("Are there any other bidders? Type 'yes' or 'no'. ")
+    if continue_bidding.lower() == 'yes':
         should_continue = True
         clear_screen()
     else:
         should_continue = False
         clear_screen()
 
-bid_winner(bid_item)
-# print("Winning bid is: ", result)
+print(bid_items)
+bid_winner(bid_items)
 
 
 
