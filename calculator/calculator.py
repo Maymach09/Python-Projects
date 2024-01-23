b3806c6d3743f7cec4b3c89b43a7f3bfdd900c80
Maymach09 <mayank.mahajan0@gmail.com>
2024-01-23 11:46:00 -0500

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,5 +1,12 @@
 from art import logo
+import os
+
 print(logo)
+
+#To clear the screen
+def clear_screen():
+    if os.name == 'nt':
+        os.system('cls')
 
 #Adding the numbers
 def add_numbers(n1, n2):
@@ -24,23 +31,27 @@ operations = {
     '/': div_numbers
 }
 
+def calculator():
+    number1 = int(input("What's the first number? "))
+        
+    for op in operations:
+        print(op)
 
-number1 = int(input("What's the first number? "))
-    
-for op in operations:
-    print(op)
+    should_continue = True
+    while should_continue:
+        symbol = input("\nSelect operator: ")
+        calc = operations[symbol]
+        number2 = int(input("\nWhat's the next number? "))
+        print("\nPick up an operation from the list below:")
+        result = calc(number1, number2)
+        print(f"\n{number1} {symbol} {number2} = {result}\n")
 
-should_continue = True
-while should_continue:
-    symbol = input("\nSelect operator: ")
-    calculator = operations[symbol]
-    number2 = int(input("\nWhat's the next number? "))
-    print("\nPick up an operation from the list below:")
-    result = calculator(number1, number2)
-    print(f"\n{number1} {symbol} {number2} = {result}\n")
+        to_continue = input("Do you want to perform more opeartions? Type 'Y' for yes or 'N' for No: ")
+        if to_continue == 'Y':
+            number1 = result
+        else:
+            should_continue = False
+            clear_screen()
+            calculator()
 
-    to_continue = input("Do you want to perform more opeartions? Type 'Y' for yes or 'N' for No: ")
-    if to_continue == 'Y':
-        number1 = result
-    else:
-        should_continue = False
+calculator()
